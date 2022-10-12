fae0ef76054afd008929401b83d1d2cc7c8a86e2
Anton <56554344+A24tx@users.noreply.github.com>
2022-10-12 11:20:30 +0300

    Update UserRepository.java

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -7,12 +7,7 @@ import ru.kata.spring.boot_security.demo.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    /**
-     * > Зачем findByUsername() когда есть loadUserByUsername()?
-     * <p>
-     * Не понял к чему этот вопрос. Разве это не методы разных слоев
-     * приложения?
-     */
+
     @EntityGraph(value = "User.roles")
     User findByUsername(String username);
 
